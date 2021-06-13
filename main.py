1dbcb979e57d63a76b70eba389ac2bed66afd3dc
JiriFranek92 <franek.j27@email.cz>
2021-06-13 13:03:25 +0200

    Drobné formátovací úpravy. Zkrácen kód na vypisování statistik.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import string as st
+
 import texts as txt
+
 
 # nastavení vstupních údajů uživatelů
 users = {"bob": "123",
@@ -7,7 +9,7 @@ users = {"bob": "123",
          "mike": "password123",
          "liz": "pass123"}
 
-DELIM = 20*"-"
+DELIM = 20 * "-"
 
 # zeptej se na vstupní údaje
 usr = input("username: ")
@@ -39,11 +41,13 @@ selection = int(selection)
 if selection < 1 or selection > len(txt.TEXTS):
     exit(f"Error! Number must be between 1 and {len(txt.TEXTS)}.")
 
-# ... jinak získej index zvoleného textu
-txt_i = selection - 1
+# ... jinak odečti 1 aby jsi získal index
+selection -= 1
 
-# vytvoř list slov očištěných od interpunkce a bílých znaků rozdělením zvoleného textu podle mezer
-word_list = [word.strip(st.whitespace + st.punctuation) for word in txt.TEXTS[txt_i].split()]
+# vytvoř list slov očištěných od interpunkce a bílých znaků
+# rozdělením zvoleného textu podle mezer
+word_list = [word.strip(st.whitespace + st.punctuation)
+             for word in txt.TEXTS[selection].split()]
 
 # vytvoř prázdný slovník pro uložení výsledků analýzy
 text_stats = {"words": 0,          # počet slov
@@ -51,51 +55,44 @@ text_stats = {"words": 0,          # počet slov
               "uppercase": 0,      # počet slov psaných velkými (SLOV)
               "lowercase": 0,      # počet slov psaných malými (slov)
               "numbers": 0,        # počet čísel
-              "number sum": 0,    # suma čísel
-              "word lengths": {}}  # slovník s četnostmi délek slov
+              "number_sum": 0,    # suma čísel
+              "word_lengths": {}}  # slovník s četnostmi délek slov
 
-# pro každé slovo v listu ...
+# pro každé slovo v listu ho připočti do statistik pokud splňuje rel. podmínku
 for word in word_list:
-    text_stats["words"] += 1  # připočti k počtu slov
-    if word.istitle():
-        text_stats["titlecase"] += 1  # (ne)připočti k počtu slov s prvním velkým
+    text_stats["words"] += 1
     if word.isupper():
-        text_stats["uppercase"] += 1  # (ne)připočti k počtu slov se všemy velkými
-    if word.islower():
-        text_stats["lowercase"] += 1  # (ne)připočti k počtu slov se všemy malými
-    if word.isnumeric():
-        text_stats["numbers"] += 1  # (ne)připočti k počtu čísel
-        text_stats["number sum"] += int(word)  # (ne)připočti číslo k sumě čísel
-    # k hodnotě slovníku 'word lengths' pro klíč {délka slova} přičti 1
-    text_stats["word lengths"][len(word)] = text_stats["word lengths"].get(len(word), 0) + 1
+        text_stats["uppercase"] += 1
+    elif word.istitle():
+        text_stats["titlecase"] += 1
+    elif word.islower():
+        text_stats["lowercase"] += 1
+    elif word.isnumeric():
+        text_stats["numbers"] += 1
+        text_stats["number_sum"] += int(word)
+    # ... a připočti délku slova do slovníku "word_lengths"
+    text_stats["word_lengths"][len(word)] = \
+        text_stats["word_lengths"].get(len(word), 0) + 1
 
 # vypiš statistiky
 if text_stats['words'] == 1:
     print(f"There is {text_stats['words']} word in the selected text.")
 else:
     print(f"There are {text_stats['words']} words in the selected text.")
-if text_stats['titlecase'] == 1:
-    print(f"There is {text_stats['titlecase']} titlecase word.")
-else:
-    print(f"There are {text_stats['titlecase']} titlecase words.")
 
-if text_stats['uppercase'] == 1:
-    print(f"There is {text_stats['uppercase']} uppercase word.")
-else:
-    print(f"There are {text_stats['uppercase']} uppercase words.")
-
-if text_stats['lowercase'] == 1:
-    print(f"There is {text_stats['lowercase']} lowercase word.")
-else:
-    print(f"There are {text_stats['lowercase']} lowercase words.")
+for case in ["titlecase", "uppercase", "lowercase"]:
+    if text_stats[case] == 1:
+        print(f"There is {text_stats[case]} {case} word.")
+    else:
+        print(f"There are {text_stats[case]} {case} words.")
 
 if text_stats['numbers'] == 1:
     print(f"There is {text_stats['numbers']} numeric string.")
 else:
     print(f"There are {text_stats['numbers']} numeric strings.")
 
-if text_stats['number sum'] > 0:
-    print(f"The sum of all the numbers is {text_stats['number sum']}.")
+if text_stats['number_sum'] > 0:
+    print(f"The sum of all the numbers is {text_stats['number_sum']}.")
 print(DELIM)
 
 # ------------------------- Tisk grafu ---------------------------------------
@@ -105,11 +102,17 @@ heading_labels = 'LEN'
 heading_values = 'OCCURENCES'
 
 # ----- pomocné proměnné pro správné formátování -----
-# šířka sloupce popisků (co je delší: délka nejdelšího popisku, nebo délka textu záhlaví)
-label_col_width = max(max([len(str(key)) for key in text_stats["word lengths"].keys()]), len(heading_labels))
-# šířka sloupce hodnot (co je delší: délka sloupce + délka textu popisku + mezera NEBO délka textu záhlaví
-values_col_width = max(max(text_stats["word lengths"].values()) +
-                       len(str(max(text_stats["word lengths"].values()))) + 1, len(heading_values))
+# šířka sloupce popisků
+# (co je delší: délka nejdelšího popisku, nebo délka textu záhlaví)
+label_col_width = \
+    max(max([len(str(key)) for key in text_stats["word_lengths"].keys()]),
+        len(heading_labels))
+# šířka sloupce hodnot
+# (co je delší: délka sloupce + délka textu popisku + mezera
+# NEBO délka textu záhlaví)
+values_col_width = max(max(text_stats["word_lengths"].values()) +
+                       len(str(max(text_stats["word_lengths"].values()))) + 1,
+                       len(heading_values))
 
 # tiskni záhlaví
 # (margin_label)POPISEK|(margin_value)HODNOTA
@@ -120,6 +123,6 @@ print(margin_label + heading_labels + "|" + margin_value + heading_values)
 print("-" * (label_col_width + values_col_width + 2))
 # tiskni jednotlivé údaje do grafu
 # (margin_label)popisek|****** (číslo)
-for label, value in sorted(text_stats["word lengths"].items()):
+for label, value in sorted(text_stats["word_lengths"].items()):
     margin_label = (label_col_width - len(str(label))) * " "
     print(margin_label + str(label) + "|" + value * "*" + " " + str(value))
